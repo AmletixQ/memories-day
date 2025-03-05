b64796e0eeeebeabc6fe9e3bd3cd0d80ad53bd56
AmletixQ <nazarovamridin308@gmail.com>
2025-03-05 16:07:34 +0300

    Update Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import Container from "../Container/Container";
 import AA from "../icons/AA";
 
 import styles from "./Header.module.scss";
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 300);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   const first_anchors = [
     { href: "#memories", text: "Главная" },
     { href: "#quote", text: "Цитаты" },
@@ -14,7 +26,9 @@ const Header = () => {
   ];
 
   return (
-    <header className={styles.navigation}>
+    <header
+      className={styles.navigation + " " + (scrolled ? styles.blur : "")}
+    >
       <Container>
         <div className={styles.navigation__parts}>
           <div className={styles.navigation__part}>
