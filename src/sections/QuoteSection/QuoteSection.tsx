75017504ec409b910893d85a476bf657bd92d8ba
AmletixQ <nazarovamridin308@gmail.com>
2025-03-07 02:01:13 +0300

    FIx header go to anchors

--- src/sections/QuoteSection/QuoteSection.tsx
+++ src/sections/QuoteSection/QuoteSection.tsx
@@ -6,45 +6,45 @@ import styles from "./QuoteSection.module.scss";
 const QuoteSection = () => {
   return (
     <Container>
-      <div id="quote" className={styles.quotes_container}>
-        <Quote>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s. 1
-          </p>
-          <Dot>1</Dot>
-        </Quote>
-        <Quote>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s. 2
-          </p>
-          <Dot>2</Dot>
-        </Quote>
-        <Quote className={styles.first}>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s.Lorem Ipsum is simply dummy text of the
-            printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard. text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard.
-          </p>
-          <Dot>3</Dot>
-        </Quote>
-        <Quote className={styles.second}>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s.Lorem Ipsum is simply dummy text of the
-            printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard.
-          </p>
-          <Dot>4</Dot>
-        </Quote>
-      </div>
+        <div id="quote" className={styles.quotes_container}>
+          <Quote>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s. 1
+            </p>
+            <Dot>1</Dot>
+          </Quote>
+          <Quote>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s. 2
+            </p>
+            <Dot>2</Dot>
+          </Quote>
+          <Quote className={styles.first}>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s.Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard. text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard.
+            </p>
+            <Dot>3</Dot>
+          </Quote>
+          <Quote className={styles.second}>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s.Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard.
+            </p>
+            <Dot>4</Dot>
+          </Quote>
+        </div>
     </Container>
   );
 };
