9dff33fb0caf5e3342b2e21c9f29af6e6525cdd8
AmletixQ <nazarovamridin308@gmail.com>
2025-03-07 01:29:57 +0300

    Create banner with marquee

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,18 @@
+import Marquee from "react-fast-marquee";
+import styles from "./Banner.module.scss";
+
+const Banner = () => {
+  return (
+    <div className={styles.banner}>
+      <Marquee speed={100}>
+        <div className={styles.banner__content}>
+          <h2>I will love you forever</h2>
+          <h2>I will love you forever</h2>
+          <h2>I will love you forever</h2>
+          <h2>I will love you forever</h2>
+        </div>
+      </Marquee>
+    </div>
+  );
+};
+export default Banner;
