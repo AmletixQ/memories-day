a481674a3da660dd9a9ca6ddf97831eda541a1fe
AmletixQ <nazarovamridin308@gmail.com>
2025-03-10 00:19:02 +0300

    Complete

--- src/sections/VideoSection/VideoSection.tsx
+++ src/sections/VideoSection/VideoSection.tsx
@@ -3,17 +3,14 @@ import Container from "../../components/Container/Container";
 import styles from "./VideoSection.module.scss";
 
 const VideoSection = () => {
+  const url = "https://youtu.be/O7PzkDur2Mo";
+
   return (
     <Container>
       <div id="video" className={styles.video}>
         <h2 className={styles.video__title}>Видеообращение</h2>
         <div className={styles.video__container}>
-          <ReactPlayer
-            url="/videos/test-video.mp4"
-            width={"100%"}
-            height={"100%"}
-            controls
-          />
+          <ReactPlayer url={url} controls width={874} height={535} />
         </div>
       </div>
     </Container>
